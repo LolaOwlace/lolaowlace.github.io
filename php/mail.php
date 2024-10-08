538210097aa93c38c25296806b2efbff4c6cee9e
Laurydube <laury.dube@gmail.com>
2024-10-08 00:45:01 -0400

    Contact page Corrections - Thank you page and mail.php setup

--- php/mail.php
+++ php/mail.php
@@ -1,32 +1,32 @@
 <?php
 $subject    = 'E-mail from example'; // Subject of your email
-$to         = 'email@example.com'; //Your e-mail address
+$to         = 'email@example.com'; // Your e-mail address
 $headers    = 'MIME-Version: 1.0' . "\r\n" .
               'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $message    = '';
 
 if (!empty($_POST["name"])) {
-  $message .= 'Name: ' . $_POST['name'] . ' <br/>';
+  $message .= 'Name: ' . htmlspecialchars($_POST['name']) . ' <br/>';
 }
 if (!empty($_POST["email"])) {
-  $message .= 'Email: ' . $_POST['email'] . ' <br/>';
+  $message .= 'Email: ' . htmlspecialchars($_POST['email']) . ' <br/>';
 }
 if (!empty($_POST["phone"])) {
-  $message .= 'Phone: ' . $_POST['phone'] . ' <br/>';
+  $message .= 'Phone: ' . htmlspecialchars($_POST['phone']) . ' <br/>';
 }
 if (!empty($_POST["website"])) {
-  $message .= 'Website: ' . $_POST['website'] . ' <br/>';
+  $message .= 'Website: ' . htmlspecialchars($_POST['website']) . ' <br/>';
 }
 if (!empty($_POST["message"])) {
-  $message .= 'Message: ' . $_POST['message'] . ' <br/>';
+  $message .= 'Message: ' . nl2br(htmlspecialchars($_POST['message'])) . ' <br/>';
 }
 
-if (@mail($to, $subject, $message, $headers))
-{
-  echo 'sent';
-}
-else
-{
-  echo 'failed';
+// Send email
+if (@mail($to, $subject, $message, $headers)) {
+  // Redirect to a thank you page
+  header('Location: /thank-you.html'); // Adjust path to point to root
+  exit; // Stop the script after redirecting
+} else {
+  echo 'Failed to send email. Please try again.';
 }
 ?>
